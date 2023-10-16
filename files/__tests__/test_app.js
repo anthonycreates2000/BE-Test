aaddedbe3c97aae0cd3d6ff5ddc0df52c523c720
MaxZx3000 <programist851@gmail.com>
2023-10-17 00:38:37 +0700

    Menambahkan unit test pada rute get livethreat attacks.

--- files/__tests__/test_app.js
+++ files/__tests__/test_app.js
@@ -7,9 +7,13 @@ const authenticatewithJWT = require("../../app/middleware/authMiddleware");
 const checkUserRole = require("../../app/middleware/roleMiddleware");
 const chai = require("chai");
 const sinon = require("sinon");
+const { expect, should } = require('chai');
 const assert = chai.assert;
 
 jest.setTimeout(15000);
+
+COUNT_DESTINATION_COUNTRY = "count_destination_country";
+COUNT_SOURCE_COUNTRY = "count_source_country";
 
 describe('test simple route hello', () => {
     test('should return a output from the endpoint', async () => {
@@ -22,5 +26,25 @@ describe('test simple route hello', () => {
             .expect(200)
 
         expect(response.body).toEqual(expected_response);
-    })
+    });
+
+    
+});
+
+describe('test rute get livethreat attacks', () => {
+    test("mendapatkan data livethreat attacks", async () => {
+      const response = await request(app)
+        .get("get_livethreat_attacks")
+        .expect(201);
+
+      const response_data = response.data;
+
+      assert.deepEqual(response_data.label, [
+        "court_destination_country",
+        "court_source_country",
+      ]);
+
+      should.ok(Number.isSafeInteger(response_data.total[0]));
+      should.ok(Number.isSafeInteger(response_data.total[1]));
+    });
 })
